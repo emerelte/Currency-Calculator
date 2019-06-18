ba5a76cb36847dc6e1bc6432141df4ee4b663a55
Maciej <mactob25@gmail.com>
2019-06-18 08:41:23 +0200

    Final

--- out/Documentation/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"currcalc","l":"AppModel"},{"p":"currcalc","l":"AppView"},{"p":"currcalc.exception","l":"CommaException"},{"p":"currcalc","l":"DataBaseDownloader"},{"p":"currcalc","l":"DataView"},{"p":"currcalc.exception","l":"NotAllowedCalculations"}]
